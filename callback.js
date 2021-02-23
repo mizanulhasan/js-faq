7ce1393035bbc0fce6ed81891cdc9beb4a6aef28
mizanulhasan <mailto.mizanul@gmail.com>
2021-02-23 22:44:55 +0600

    Callback Function Practice

--- callback.js
+++ callback.js
@@ -1,19 +1,39 @@
-function explain_callback(name, age, task){
-    task();
-    console.log('Hello ', name);
-    console.log('Age ', age);
-    
+function welcomeGuest(name, greetHandler){
+    greetHandler(name);
+}
+const actorName = 'Tom Hanks';
+
+function greetMorning(name){
+    console.log('Good Morning', name);
 }
 
-function washHand(){
-    console.log('Wash hand with soap');
+function greetEvening(name){
+    console.log('Good Evening', name);
 }
-function takeShower(){
-    console.log('Take Shower');
+
+function greetAfternoon(name){
+    console.log('Good Afternoon', name);
 }
-function scrollFacebook(){
-    console.log('Scroll Facebook but dont like any posts');
-}
-explain_callback('Sogir Uddin', 17, washHand);
-explain_callback('Kogir Uddin', 13, takeShower);
-explain_callback('Mugir Uddin', 21,scrollFacebook);
+welcomeGuest(actorName, greetEvening);
+welcomeGuest('Johny Depp', greetAfternoon);
+welcomeGuest('Jack Sparrow', greetMorning);
+
+// function explain_callback(name, age, task){
+//     task();
+//     console.log('Hello ', name);
+//     console.log('Age ', age);
+    
+// }
+
+// function washHand(){
+//     console.log('Wash hand with soap');
+// }
+// function takeShower(){
+//     console.log('Take Shower');
+// }
+// function scrollFacebook(){
+//     console.log('Scroll Facebook but dont like any posts');
+// }
+// explain_callback('Sogir Uddin', 17, washHand);
+// explain_callback('Kogir Uddin', 13, takeShower);
+// explain_callback('Mugir Uddin', 21,scrollFacebook);
